94e2b1f163e1a25a11d4ccd12b9d17c8aebf20fc
SaraJhulian <sarajhulian@gmail.com>
2022-05-17 16:06:56 -0400

    3 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import './style.css';
 
+import { ComplaintListRow } from './components/ComplaintListRow';
+
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ComplaintListRow />
     </div>
   );
 }
---
+++ components/ComplaintListRow/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const ComplaintListRow = () => {
+  return <div>ComplaintLIstRow</div>;
+};
